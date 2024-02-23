77d9a147582943346efb800524e891eed77a57cc
FFGGSSJJ <fuguan@student.ethz.ch>
2024-02-23 20:32:38 +0100

    upload README and ignore

--- scripts-part1/local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# scripts for local machine to perform data analysis
-
-# download latests measure results from node
-gcloud compute scp --recurse --ssh-key-file $GCLOUD_SSH ubuntu@client-measure-xsgq:/home/ubuntu/results ./results --zone europe-west3-a
-
