b4535abac34c956cec9ccd5261c6ae9987072028
FFGGSSJJ <fuguan@student.ethz.ch>
2024-03-17 21:37:19 +0100

    chore: update script

--- scripts-part1/mctest-0.sh
+++ scripts-part1/mctest-0.sh
@@ -3,6 +3,8 @@ echo 'MEMCACHED_IP: ' $MEMCACHED_IP
 echo 'INTERNAL_AGENT_IP:' $INTERNAL_AGENT_IP
 
 RESULT_DIR='/home/ubuntu/results/'
+TEST_NAME=$1
+
 if [ ! -d $RESULT_DIR ] 
 then
 	mkdir $RESULT_DIR
@@ -11,7 +13,7 @@ fi
 for i in 1 2 3
 do
 	echo 'test-'$i
-	RESULT_NAME='results/'$1'-'$i'.txt'
+	RESULT_NAME='results/'$TEST_NAME'-'$i'.txt'
 	TMP_NAME='results/tmp.txt'
 	./memcache-perf/mcperf -s $MEMCACHED_IP -a $INTERNAL_AGENT_IP --noload -T 16 -C 4 -D 4 -Q 1000 -c 4 -t 5 -w 2 --scan 5000:55000:5000 | tee $RESULT_NAME
 	head -n -2 $RESULT_NAME > $TMP_NAME; mv $TMP_NAME $RESULT_NAME;
